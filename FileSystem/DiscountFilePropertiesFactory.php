e2c24967bf999328af6fa5c87477dbf76e033efb
Mario Quesada <centralmqmtech@gmail.com>
2012-06-12 18:23:16 +0200

    refactored to support deletion of the portal_discount_rule

--- FileSystem/DiscountFilePropertiesFactory.php
+++ FileSystem/DiscountFilePropertiesFactory.php
@@ -8,18 +8,35 @@ final class DiscountFilePropertiesFactory
 {
     private $properties;
     private $propertiesPath;
+    private $propertiesFallbackPath;
     
-    public function __construct(PropertiesInterface $properties, $propertyFilename)
+    public function __construct(PropertiesInterface $properties, $propertyFilename, $propertiesFallbackPath)
     {
         $this->properties = $properties;                
         $this->propertiesPath = $propertyFilename;
+        $this->propertiesFallbackPath = $propertiesFallbackPath;
     }
     
     public function createProperties()
     {
-        $absolutePath = __DIR__ . '/../' . $this->propertiesPath;
-        $this->properties->parse($absolutePath);
+        try {
+            $absolutePath = __DIR__ . '/../' . $this->propertiesPath;
+            $this->properties->parse($absolutePath);
+        }
+        catch (\Exception $e){
+            $this->resetProperties();
+            $this->properties->parse($absolutePath);
+        }
         
         return $this->properties;
+    }
+    
+    private function resetProperties()
+    {
+        $absolutePath = __DIR__ . '/../' . $this->propertiesFallbackPath;
+        $contents = file_get_contents($absolutePath);
+        
+        $absolutePath = __DIR__ . '/../' . $this->propertiesPath;
+        file_put_contents($absolutePath, $contents);
     }
 }
